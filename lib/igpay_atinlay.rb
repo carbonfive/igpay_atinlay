f3d3de0be1309211ad7ce88ec025a66257d6dcff
Jared Carroll <jared@carbonfive.com>
2011-07-18 17:17:06 +0000

    Reverse Pig Latin Friendly Check
    "if !" is not idiomatic Ruby. It should be replaced with
    "unless". However in the case of an "if !" followed by an
    "else" the conditions should be swapped instead of using
    "unless". Using "unless" would have resulted in "unless...else"
    which is strictly prohibited.

--- lib/igpay_atinlay.rb
+++ lib/igpay_atinlay.rb
@@ -1,15 +1,15 @@
 String.class_eval do
   def to_pig_latin
     self.split(/(\-|\s+)/).collect do |word|
-      if ! word.pig_latin_friendly?
-        word
-      else
+      if word.pig_latin_friendly?
         word.match /^(qu|[^aeiou]*)(.+?)([^a-z]*)$/i do |m|
           first, punc = m[2] + m[1], m[3]
           first.capitalize! if word =~ /^[A-Z]/
           last = (first.downcase == 'a' ? 'y' : 'ay')
           first + last + punc
         end
+      else
+        word
       end
     end.join
   end
