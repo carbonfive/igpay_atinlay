1af62a356cd6d1fdf7992df4c03d484c1ed855b2
Jared Carroll <jared@carbonfive.com>
2011-07-18 18:22:57 +0000

    Consistently Format Tests with Non-Test Code

--- test/igpay_atinlay_test.rb
+++ test/igpay_atinlay_test.rb
@@ -7,54 +7,65 @@ class IglatinpayTest < MiniTest::Spec
       'hello'.to_pig_latin.must_equal 'ellohay'
     end
   end
+
   describe 'multiple words' do
     it 'splits the words on whitespace' do
       'hello world'.to_pig_latin.must_equal 'ellohay orldway'
     end
   end
+
   describe 'capitalization' do
     it 'keeps the first letter capitalized' do
       'Hello World'.to_pig_latin.must_equal 'Ellohay Orldway'
     end
   end
+
   describe 'punctuation at the end of a word' do
     it 'keeps any punctuation at the end' do
       'Hello, World!!'.to_pig_latin.must_equal 'Ellohay, Orldway!!'
     end
   end
+
   describe 'punctuation within a word' do
     it 'keeps punctuation within the word' do
       "Mike's computer".to_pig_latin.must_equal "Ike'smay omputercay"
     end
   end
+
   describe 'multiple consonants at the beginning of a word' do
     it 'keeps consonants at the beginning of a word together' do
       'Quick brown fox'.to_pig_latin.must_equal 'Ickquay ownbray oxfay'
     end
   end
+
   describe 'vowels at the beginning of a word' do
     it 'keeps vowels at the beginning of the word intact' do
       'eat apples'.to_pig_latin.must_equal 'eatay applesay'
     end
   end
+
   describe 'whitespace' do
     it 'splits on all whitespace and keeps it intact' do
       'Hello...  world!'.to_pig_latin.must_equal 'Ellohay...  orldway!'
     end
   end
+
   describe 'non-word words' do
     it 'does not translate non-word words' do
       'hello 12345 abc123 ab123cd world'.to_pig_latin.must_equal 'ellohay 12345 abc123 ab123cd orldway'
     end
   end
+
   describe 'edge cases' do
     it 'converts "a" to "ay"' do
       'a'.to_pig_latin.must_equal 'ay'
     end
+
     it 'converts "A" to "Ay"' do
       "A dog".to_pig_latin.must_equal 'Ay ogday'
     end
   end
+
   describe 'hyphens' do
     it 'splits hyphenated words up' do
       'rob is a bad-ass'.to_pig_latin.must_equal 'obray isay ay adbay-assay'
